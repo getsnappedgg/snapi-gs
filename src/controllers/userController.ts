fcc158a148e226c96b02dd42a0999bcb26d68662
a-hsu <a.hsu873@gmail.com>
2022-12-18 09:57:27 -0800

    changed passSalt type, added authentication to userController

---
+++ src/controllers/userController.ts
@@ -0,0 +1,87 @@
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcryptjs");
+const asyncHandler = require("express-async-handler");
+import { prisma } from "../index";
+
+// @desc    Register new user
+// @route   POST /api/users
+// @access  Public
+const registerUser = asyncHandler(async (req: any, res: any) => {
+	const { name, email, password } = req.body;
+
+	if (!name || !email || !password) {
+		res.status(400);
+		throw new Error("Please add all fields");
+	}
+
+	// Check if user exists
+	const userExists = await prisma.user.findUnique({
+		where: { email },
+	});
+
+	if (userExists) {
+		res.status(400);
+		throw new Error("User already exists");
+	}
+
+	// Hash password
+	const passSalt = 10;
+	const salt = await bcrypt.genSalt(passSalt);
+	const hashedPassword = await bcrypt.hash(password, salt);
+
+	// Create user
+	const user = await prisma.user.create({
+		data: {
+			name,
+			email,
+			passSalt,
+			passHash: hashedPassword,
+		},
+	});
+
+	if (user) {
+		res.status(201).json({
+			id: user.id,
+			name: user.name,
+			email: user.email,
+			token: generateToken(user.id),
+		});
+	} else {
+		res.status(400);
+		throw new Error("Invalid user data");
+	}
+});
+const loginUser = asyncHandler(async (req:any, res:any) => {
+	const { email, password } = req.body;
+
+	// Check for user email
+	const user = await prisma.user.findUnique({ where: { email } });
+
+	if (user && (await bcrypt.compare(password, user.passHash))) {
+		res.json({
+			_id: user.id,
+			name: user.name,
+			email: user.email,
+			token: generateToken(user.id),
+		});
+	} else {
+		res.status(400);
+		throw new Error("Invalid credentials");
+	}
+});
+
+const getMe = asyncHandler(async (req: any, res: any) => {
+	res.status(200).json(req.user);
+});
+
+const generateToken = (id: number) => {
+	return jwt.sign({ id }, process.env.JWT_SECRET, {
+		expiresIn: "30d",
+	});
+};
+
+module.exports = {
+	registerUser,
+	loginUser,
+	getMe,
+};
--- src/models/userModel.ts
+++ src/models/userModel.ts
@@ -1,3 +1,24 @@
-
-// export const User
-//
+export type User = {
+	id: number;
+	name: string;
+	email: string;
+	// decks: Deck[]
+	// role: Role
+	/*
+    id       Int    @id @default(autoincrement())
+  name     String
+  email    String
+  passSalt Int
+  passHash String
+  decks    Deck[]
+  role     Role   @default(USER)
+ */
+};
+export const build = (data: any) => {
+	const { id, name, email } = data;
+	return {
+		id,
+		name,
+		email,
+	} as User;
+};
