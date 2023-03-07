1338fa58bdec73aa8df6e6ad10d024187c2d3070
a-hsu <a.hsu873@gmail.com>
2023-03-07 12:37:04 -0500

    changed dist path

--- dist/src/constants/redis.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.REDIS_CONSTANTS = void 0;
-exports.REDIS_CONSTANTS = {
-    CARD_LIST: "card-list",
-    LAST_UPDATED: "last-updated",
-};
-//# sourceMappingURL=redis.js.map
